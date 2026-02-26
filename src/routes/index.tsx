f70418b52d8c21b7c196bd01c8e22c0090ca7d04
MacroMelon <MacroMelon>
2026-02-26 14:21:49 +0000

    add stuff below video

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,11 @@
 import { createFileRoute } from '@tanstack/react-router'
-import deviceImg1 from './../assets/home-page/airpods-pro-birebir-bluetooth-kulaklik-f49-ee 2.png'
-import deviceImg2 from './../assets/home-page/image 96 2.png'
-import deviceImg3 from './../assets/home-page/img_0197-1729523853862 3.png'
-import deviceImg4 from './../assets/home-page/laptop_586 2.png'
-import deviceImg5 from './../assets/home-page/mobile-phones-buying-guide 1.png'
+import deviceImg1 from '../assets/home-page/devices/airpods-pro-birebir-bluetooth-kulaklik-f49-ee 2.png'
+import deviceImg2 from '../assets/home-page/devices/image 96 2.png'
+import deviceImg3 from '../assets/home-page/devices/img_0197-1729523853862 3.png'
+import deviceImg4 from '../assets/home-page/devices/laptop_586 2.png'
+import deviceImg5 from '../assets/home-page/devices/mobile-phones-buying-guide 1.png'
+
+import bewareImg from '../assets/home-page/image 137-1.png'
 
 export const Route = createFileRoute('/')({ component: App })
 
@@ -50,6 +52,43 @@ function App() {
                 <source src="/video.mp4" type="video/mp4"/>
                 video
             </video>
+            <div className="py-72 px-24 gap-12">
+                <div className="flex h-[29rem]">
+                    <img
+                        className="h-full"
+                        src={bewareImg}
+                        alt="Sign warning people to beware of pickpockets"
+                    />
+                    <div className="ml-32 flex flex-col">
+                        <div className="font-koulen text-[#3D4EE8] tracking-wide text-[4rem]">
+                            You are not alone
+                        </div>
+                        <div className="mt-4 pr-20 text-[2.6rem]/12 tracking-wide font-inter text-[#383838]">
+                            Thieves are ever present, and many of us will experience theft at some point, sometimes more than once. It is not something to be ashamed of, but something that invites awareness and care.
+                        </div>
+                        <div className="flex mt-auto gap-10">
+                            <a
+                                className="rounded-full flex align-middle justify-center border-5 border-[#555FD0] py-2 px-20"
+                            >
+                                <div className="font-inter text-[#555FD0] text-4xl pb-1">
+                                    Get your Tether
+                                </div>
+                            </a>
+                            <a
+                                className="rounded-full flex align-middle justify-center bg-[#394293] py-2 px-7"
+                                href="/blog"
+                            >
+                                <div className="font-inter text-white text-4xl pt-1">
+                                    More about pickpocketing
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div>
+
+                </div>
+            </div>
         </div>
     )
 }
